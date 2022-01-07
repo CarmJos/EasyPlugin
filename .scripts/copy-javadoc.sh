b1b6b95a07bcf83a5d0ef832b0c76d02e6c12689
CarmJos <KarmunJ@outlook.com>
2022-01-08 01:47:49 +0800

    测试Javadoc部署

---
+++ .scripts/copy-javadoc.sh
@@ -0,0 +1,11 @@
+rm -rf docs
+mkdir -vp docs
+
+for i in ../easyplugin*; do
+  if test -e "$i/target/apidocs/"; then
+    cp -vrf "$i/target/apidocs/*" "docs/$1/"
+  fi
+done
+
+cp -vrf .documentation/javadoc/JAVADOC-README.md docs/README.md
+cp -vrf .documentation/javadoc/index.html docs/index.html
