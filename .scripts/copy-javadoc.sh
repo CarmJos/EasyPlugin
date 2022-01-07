40b66740e0de81106e717cf312d092236cf6d3ca
CarmJos <KarmunJ@outlook.com>
2022-01-08 02:31:44 +0800

    测试Javadoc部署

--- .scripts/copy-javadoc.sh
+++ .scripts/copy-javadoc.sh
@@ -6,17 +6,14 @@ DOC_URL="target/site/apidocs"
 for FILE_NAME in easyplugin-*; do
 
   if test -e "$FILE_NAME/$DOC_URL"; then
-    ls -l "$FILE_NAME"
 
     MODULE_FILE="docs/${FILE_NAME:11}/"
 
     mkdir -vp "$MODULE_FILE"
 
-    cp -vrf "$FILE_NAME/$DOC_URL/*" "$MODULE_FILE"
+    cp -vrf "$FILE_NAME"/"$DOC_URL"/* "$MODULE_FILE"
 
   fi
-
-  echo "$FILE_NAME"
 
 done
 
