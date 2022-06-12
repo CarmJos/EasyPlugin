01b27e25bbc0c66a222e1c4d71ee2ff753bd1a6b
CarmJos <KarmunJ@outlook.com>
2022-06-12 23:28:34 +0800

    feat(command): 新增可快捷return的消息方法。

--- base/command/src/main/java/cc/carm/lib/easyplugin/command/NamedExecutor.java
+++ base/command/src/main/java/cc/carm/lib/easyplugin/command/NamedExecutor.java
@@ -1,8 +1,16 @@
 package cc.carm.lib.easyplugin.command;
 
+import cc.carm.lib.mineconfiguration.bukkit.value.ConfiguredMessage;
+import cc.carm.lib.mineconfiguration.bukkit.value.ConfiguredMessageList;
+import org.bukkit.command.CommandSender;
 import org.bukkit.permissions.Permissible;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 public interface NamedExecutor {
 
@@ -13,5 +21,33 @@ public interface NamedExecutor {
     default boolean hasPermission(Permissible permissible) {
         return true;
     }
+
+    default Void sendMessage(@NotNull CommandSender sender, @NotNull String... messages) {
+        return sendMessage(sender, (Function<String, String>) null, messages);
+    }
+
+    default Void sendMessage(@NotNull CommandSender sender,
+                             @Nullable Function<String, String> parser,
+                             @NotNull String... messages) {
+        if (messages == null || messages.length == 0) return null;
+        Function<String, String> finalParser = Optional.ofNullable(parser).orElse(Function.identity());
+        Arrays.stream(messages).map(finalParser).forEach(sender::sendMessage);
+        return null;
+    }
+
+    default Void sendMessage(@NotNull CommandSender sender,
+                             @NotNull ConfiguredMessageList<?> messages,
+                             @Nullable Object... params) {
+        messages.send(sender, params);
+        return null;
+    }
+
+    default Void sendMessage(@NotNull CommandSender sender,
+                             @NotNull ConfiguredMessage<?> message,
+                             @Nullable Object... params) {
+        message.send(sender, params);
+        return null;
+    }
+
 
 }
