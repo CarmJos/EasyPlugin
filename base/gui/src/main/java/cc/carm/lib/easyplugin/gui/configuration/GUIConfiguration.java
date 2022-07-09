b7e92956858d70f5053a76f03df00c7e54612ce7
carm <Karmun@foxmail.com>
2022-07-10 04:58:59 +0800

    chore(gui): 为 GUIConfiguration 添加 serialize() 方法。

--- base/gui/src/main/java/cc/carm/lib/easyplugin/gui/configuration/GUIConfiguration.java
+++ base/gui/src/main/java/cc/carm/lib/easyplugin/gui/configuration/GUIConfiguration.java
@@ -6,19 +6,26 @@ import cc.carm.lib.easyplugin.utils.ColorParser;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class GUIConfiguration {
 
-    String title;
-    int lines;
+    protected String title;
+    protected int lines;
 
-    List<GUIItemConfiguration> guiItems;
+    protected Map<String, GUIItemConfiguration> guiItems;
 
-    public GUIConfiguration(String title, int lines, List<GUIItemConfiguration> guiItems) {
+    public GUIConfiguration(String title, int lines) {
+        this(title, lines, new LinkedHashMap<>(1));
+    }
+
+    public GUIConfiguration(String title, int lines, Map<String, GUIItemConfiguration> guiItems) {
         this.title = title;
         this.lines = lines;
         this.guiItems = guiItems;
@@ -38,29 +45,48 @@ public class GUIConfiguration {
                 .get();
     }
 
-    public List<GUIItemConfiguration> getGuiItems() {
+    public Map<String, GUIItemConfiguration> getGUIItems() {
         return guiItems;
     }
 
     public void setupItems(Player player, GUI gui) {
-        getGuiItems().forEach(itemConfiguration -> itemConfiguration.setupItems(player, gui));
+        getGUIItems().values().forEach(itemConfiguration -> itemConfiguration.setupItems(player, gui));
+    }
+
+    public @NotNull Map<String, Object> serialize() {
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+
+        map.put("title", this.title);
+        map.put("lines", this.lines);
+        if (!this.guiItems.isEmpty()) {
+            LinkedHashMap<String, Object> items = new LinkedHashMap<>();
+            this.guiItems.forEach((key, value) -> items.put(key, value.serialize()));
+            map.put("items", items);
+        }
+        return map;
     }
 
 
     public static GUIConfiguration readConfiguration(@Nullable ConfigurationSection section) {
-        if (section == null) return new GUIConfiguration("name", 6, new ArrayList<>());
-
-        String title = section.getString("title", "");
-        int lines = section.getInt("lines", 6);
-        ConfigurationSection itemsSection = section.getConfigurationSection("items");
-        if (itemsSection == null) return new GUIConfiguration(title, lines, new ArrayList<>());
+        if (section == null) return new GUIConfiguration("name", 6);
 
         return new GUIConfiguration(
-                title, lines, itemsSection.getKeys(false).stream()
-                .map(key -> GUIItemConfiguration.readFrom(itemsSection.getConfigurationSection(key)))
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList())
+                section.getString("title", ""),
+                section.getInt("lines", 6),
+                readItems(section.getConfigurationSection("items"))
         );
+    }
+
+    public static Map<String, GUIItemConfiguration> readItems(ConfigurationSection itemsSection) {
+        Map<String, GUIItemConfiguration> items = new LinkedHashMap<>();
+        if (itemsSection == null) return items;
+
+        for (String key : itemsSection.getKeys(false)) {
+            GUIItemConfiguration item = GUIItemConfiguration.readFrom(itemsSection.getConfigurationSection(key));
+            if (item != null) items.put(key, item);
+        }
+
+        return items;
     }
 
     public static ClickType readClickType(String type) {
